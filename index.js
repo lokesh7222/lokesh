b7a8a07cfd61ff7a0aa29dbb5704d3dc6192ced0
lokesh7222 <singh.lokesh7222@gmail.com>
2024-01-11 12:19:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,404 @@
+// const accountId = 11233
+// let accountEmail = "lokesh@gmail.com"
+// var accountPass = "122344"
+// accountCity = "jaipur"
+
+
+// // accountId=7654
+// accountEmail="kuhgfs@gmail"
+// accountPass="9876"
+// accountCity="bharatpur"
+
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPass,accountCity])
+
+// let score = "33ytr"
+// console.log(typeof score)
+//  let value = Number(score)
+//  console.log(value);
+
+// console.log("2">1);
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);
+// console.log(undefined ==0);
+// console.log(undefined =0);
+// console.log(undefined >=0);
+
+// console.log("2"===2);
+
+// let myYoutube="lokeshkuntal"
+// let anotherName="myYoutube"
+// anotherName="chauaurcode"
+
+// console.log(myYoutube);
+// console.log(anotherName);
+
+// let userOne ={
+//     email:"user@gmail.com",
+//     upi:"user@ybl"
+// }
+
+// let userTwo=userOne
+// userTwo.email="lokesh@gmail.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+// let text = "lokesgsinghkuntalwalaanaj"
+// console.log(text.length)
+
+// let myString = "Helloworldkuntal";
+// console.log(myString.length); 
+
+//  let a="lokeshkunta"
+//  console.log(a.charAt(1));
+//  let str = "lokesg@" 
+// console.log(str.charCodeAt(0));
+//  let text="apple,banana,kiwi"
+//  console.log(text.substring(7,13));
+
+//  let stre="helloworld,lokesh,sinfh"
+//  console.log(stre.toUpperCase());
+
+
+//  let str1 = "Hello";
+//  let str2 = "World";
+//  let result = str1.concat(" ", str2);
+ 
+//  console.log(result); // Output: Hello, World
+
+
+
+// let text="   lokesh ,singh,kunta   "
+// console.log(text.trimStart());
+
+// let text1="   lokesh ,singh,kunta   "
+// console.log(text1.trimEnd());
+
+// var str = "598799";
+// var paddedStr = str.padStart(3, "0");
+
+// console.log(paddedStr); // Output: "005"
+
+// var originalString = "Hello, World!";
+// var newString = originalString.replace("World", "Universe");
+
+// console.log(newString); // Output: Hello, Universe!
+
+
+// let text = "Please visit Microsoft and Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText);
+
+
+// var fruits = "apple,orange,banana";
+// var fruitsArray = fruits.split(",");
+
+// console.log(fruitsArray); // Output: ["apple", "orange", "banana"]
+
+// var str = "Hello, World!";
+// var index = str.indexOf("World");
+
+// console.log("The index of 'World' is: " + index); // Output: The index of 'World' is: 7
+
+// let balance = new Number(100)
+// console.log(balance);
+// let score=100
+// // console.log(score.toFixed(4));
+
+// let otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(5));
+
+// let hundreds=10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// let x=129.656
+// console.log(x.toExponential(2));
+// // console.log(x.toExponential(4));
+// // console.log(x.toExponential(6));
+
+// let math=3.7
+
+//  console.log(Math.round(8.3));
+//  console.log(Math.ceil(-8.9));
+//  console.log(Math.floor(8.5));
+// //  console.log(Math.round(8.3));
+// console.log(Math.trunc(8.5));
+// console.log(Math.trunc(5.4));
+// console.log(Math.trunc(7.9));
+// console.log(Math.trunc(-8.5));
+// console.log(Math.trunc(8.5));
+
+// console.log(Math.sign(-3));
+// console.log(Math.sign(88.76));
+// console.log(Math.sign(0));
+// console.log(Math.pow(8,3));
+
+// console.log(Math.sqrt(4));
+// console.log(Math.min(-3,3,4,5,66,78,));
+
+// console.log(Math.abs(7));
+
+// console.log(Math.random(-9));
+
+
+// console.log(Math.log(8));
+// console.log(Math.log2(8));
+// console.log(Math.log10(8));
+
+
+// console.log(Math.sin(90 * Math.PI / 1800));
+
+// console.log(Math.cos(0 * Math.PI / 180));
+
+// let d=new Date();
+// console.log(d);
+
+// const cars = ["BMW","AUDIO","VOLVO"];
+// console.log(cars[1]);
+
+// const myHeros = [1,2,3,4,5,6]
+// console.log(myHeros[4]);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.push("Kiwi");
+// console.log(fruits);
+
+// const arr =[1,2,3,4,5]
+// arr.pop()
+// // console.log(arr);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift();
+// console.log(fruits);
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Lemon");
+// console.log(fruits);
+
+// const fruits = ["Banana", "Apple", "kiwi"];
+// fruits.splice(2, 2,"Lemon", "orange");
+// console.log(fruits);
+
+// const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 2, "Lemon", "Kiwi");
+// console.log(fruits1);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(0, 1);
+// console.log(fruits);
+
+
+// let arr = [1,2,3,4,5,6,7]
+// arr.splice(0,4)
+// console.log(arr);
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(3);
+// console.log(citrus);
+
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus);
+
+// learn others 
+// const arr = [1,2,3,4,5,6,7];
+// arr.slice(1,4);
+// console.log(arr);
+
+
+// let y =[1,2,3,4,5,6,7];
+// x.splice(1,4);
+// console.log(y);
+
+
+// let text = "apple,banana,kiwi"
+// console.log(text.slice(2,5))
+// let score = "apple,banana,kiwi"
+// console.log(score.substr(2,5));
+// let arr = "apple,banana,kiwi"
+// console.log(arr.substring(2,5));
+
+// let text1 = "apple,banana,kiwi"
+// console.log(text1.slice(7,13))
+// let score1 = "apple,banana,kiwi"
+// console.log(score1.substr(7,10));
+// let arr1 = "apple,banana,kiwi"
+// console.log(arr1.substring(7,3));
+
+// let score2 = "apple,banana,kiwi"
+// console.log(score2.substr(7,3));
+
+
+// let text = ["banana","orange","lemon","apple","mango"]
+// let citrus = text.slice(2);
+// console.log(citrus);
+
+
+// let text1 = ["banana","orange","lemon","apple","mango"]
+// let citrus1 = text1.slice(1,3);
+// console.log(citrus1);
+
+
+// let text2 = ["banana","orange","lemon","apple","mango"]
+// let cit2 = text2.splice(2);
+// console.log(cit2);
+
+
+// let text3 = ["banana","orange","lemon","apple","mango"]
+// let citrus3 = text3.splice(1,3);
+// console.log(citrus3);
+
+// let text4 = ["banana","orange","lemon","apple","mango"]
+// text4.splice(2)
+// console.log(text4);
+
+
+// let text5 = ["banana","orange","lemon","apple","mango"]
+// text5.splice(1,3);
+// console.log(text5);
+
+// let fruits2 = ["Apple", "Orange", "kiwi", "Mango"];
+// let position1 = fruits2.indexOf("kiwi") ;
+// console.log(position1);
+
+
+// const fruits = ["Apple", "Orange", "Apple", "Mango"];
+// let position = fruits.indexOf("Apple") ;
+// console.log(position);
+
+// const fruits1 = ["Apple", "Orange", "Apple", "Mango"];
+// fruits1.indexOf("Apple") ;
+// console.log (fruits1);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.includes("Mango");
+// console.log(fruits);
+
+// const numbers = [4, 9, 16, 25, 29 ,39 ,47];
+// let first = numbers.findIndex(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 30;
+// }
+// console.log(first);
+
+// const numbers1 = [4, 9, 16, 25, 29];
+// let first1 = numbers.find(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+// console.log(first1);
+
+
+// const temp = [27, 28, 30, 40, 42, 35, 30];
+// let high = temp.findLast(x => x > 40);
+// console.log(high);
+
+
+// const temp1 = [27, 28, 30, 40, 42, 35, 30,41,44,56,56,67,78,99,87,86,56];
+// let high1 = temp1.findLast(x => x > 40);
+// console.log(high1);
+
+// const temp2 = [27, 28, 30, 40, 42, 35, 30,41,4,45,99,87,76];
+// let pos2 = temp2.findLast(x => x > 40);
+// console.log(pos2);
+
+// let heros =["sunil","satish","lokesh","ram"]
+// let heros1 =["urvi","khushi","lavi","seeta"]
+// heros.push(heros1)
+// console.log(heros);
+
+// let allHero = heros.concat(heros1);
+// console.log(allHero);
+// let newallhero = [...heros,...heros1]
+// console.log(newallhero);
+
+// let another_array =[1,2,3,[4,5,6],7,[6,7[4,5]]]
+// let new_array = another_array.flat(Infinity)
+// console.log(new_array);
+
+
+// console.log (Array.isArray("lokesh"))
+// console.log(Array.fromArray("lokesh"));
+
+// let score = 100
+// let score2=200
+// let score3 = 300
+// console.log(Array.of (score,score2,score2));
+
+const myArr = [1, 2, 3, 4, 5, 6];
+const newArr = myArr.flatMap((x) => x * 2);
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
