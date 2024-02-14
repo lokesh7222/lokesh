d3ca63fee4c705878dc08496d9440b2d3227ba65
lokesh7222 <singh.lokesh7222@gmail.com>
2024-02-14 14:09:56 +0530

    added

--- index.js
+++ index.js
@@ -335,60 +335,963 @@
 // let score3 = 300
 // console.log(Array.of (score,score2,score2));
 
-const myArr = [1, 2, 3, 4, 5, 6];
-const newArr = myArr.flatMap((x) => x * 2);
-console.log(newArr);
+// const myArr = [1, 2, 3, 4, 5, 6];
+// const newArr = myArr.flatMap((x) => x * 2);
+// console.log(newArr);
+
+
+// const person = {
+//     firstName:"John",
+//     lastName:"Doe",
+//     age:50, eyeColor:"blue"
+   
+//   }
+  
+//   const x = person;
+//   x.age = 10; 
+//   console.log(delete person.age);
+//   console.log(person.nationality = "English");
+//   console.log(person);
+//   console.log(person.lastName);
+//   console.log(person.firstName + " is " + person.age + " years old.");
+//   console.log(person["firstName"] + " is " + person["age"] + " years old.");
+
+
+// let myObj = {
+//     name:"John",
+//     age:30,
+//     cars: {
+//       car1:"Ford",
+//       car2:"BMW",
+//       car3:"Fiat"
+//     }
+//   }
+// console.log(myObj.cars.car2);
+
+// const myObj = {
+//     name: "John",
+//     age: 30,
+//     cars: [
+//       {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+//       {name:"BMW", models:["320", "X3", "X5"]},
+//       {name:"Fiat", models:["500", "Panda"]}
+//     ]
+//   }
+//   for (let i in myObj.cars) {
+//     x += "<h1>" + myObj.cars[i].name + "</h1>";
+//     for (let j in myObj.cars[i].models) {
+//     x += myObj.cars[i].models[j];
+//     }
+//   }
+
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     person_name : function () {
+//         return (this.firstName + " " + this.lastName).toUpperCase();
+//       }
+//   };
+
+//   console.log(person.person_name());
+
+// let x= 3;
+// let y= 4;
+function myFunction(a,b){
+    return a*b;
+}
+
+// // // Function is called, the return value will end up in x
+// let m = myFunction(5, 3);
+// // console.log(m);
+
+// function myFunction(a, b) {
+// // Function returns the product of a and b
+//   return a * b;
+// }
+// console.log(m);
+
+
+// // Function is called, the return value will end up in x
+// let x = myFunction(4, 3);
+
+// function myFunction(a, b) {
+// // Function returns the product of a and b
+//   return a * b;
+// }
+// console.log(x);
+
+
+// function myName() {
+//     console.log("l");
+//     console.log("o");
+//     console.log("k");
+//     console.log("e");
+//     console.log("s");
+    
+// }
+
+// myName()
+
+
+// function addTwoNumber (a,b){
+//     return a+b;
+// }
+ 
+
+// function one() {
+//     console.log(this);
+    
+// }
+// one()
+
+// const addTwo =  (num1,num2) => {
+//     return num1+num2
+// }
+
+// console.log(addTwo(3,4));
+// // 
+
+
+// (function chai (){
+//     console.log(`db connected`);
+// })();
+
+// (   () => {
+//     console.log(`db connected`);
+// })();
+
+// (   (name) => {
+//     console.log(`db connected ${name}` );
+// })(`lokesh`);
+
+
+// const month =6
+
+// switch (month) {
+//     case 1:
+//     console.log("jan");
+//         break;
+// case 2: 
+// console.log("fer");
+//      break;
+//      case 3:
+//  console.log("march");
+//      break;
+//     default:
+//         console.log("match");
+//         break;
+// }
 
 
 
+// let val1;
+// val1 = 4 ?? 12
+// val1 = null ?? 12
+// val1 = undefined ?? 12
+// val1 = null?? 4 ?? 12
+// console.log(val1);
 
 
 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     console.log('lokej');
+    
+// // }
+
+// for (let i = 5; i <= 50; i = i+5 ) {
+//     console.log(i)
+// }
+// for (let i = 0; i < 50; i++ ) {
+//     if(i%2 !=0)
+//     console.log(i)
+// }
+// for (let i = 0; i < 50; i++ ) {
+//     if(i%2==0)
+//     console.log(i)
+// }
 
 
+// let i=0
+// while (i<10) {
+//     console.log('lokesh')
+//     i++ 
+// // }
+// let l=0
+// while (l<10) {
+//     console.log('lokesh n')
+     
+// }
+// var geek = ['lokesh','satish','ram','seeta',99];
+// geek.forEach(function (name) {
+//     console.log(name)
+// })
+
+// let a=10;
+// let b = 1;
+// if (a>b) {
+//     console.log('a is greater than');
+    
+// }
 
 
+// let age=13;
+// if(age>=18){
+//     console.log('you can vite');
+// } else{
+//     console.log('you can not vote');
+// }
+
+// let age=19;
+// if(age>=18){
+//     console.log('you can vote');
+// } else if(age==17){
+//     console.log('please wait one year');
+// } else{
+//     console.log('you can not vote');
+// }
 
 
+// function sayHello(){
+//     console.log('hello');
+// }
+// function add(num1,num2,callback){
+//     console.log(num1+num2);
+//     callback();
+// }
+// let a =10;
+// let b =20;
+// add(a,b,sayHello);
+// add(20,38,function(){
+//     console.log('bye...');
+// });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let arr=[1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
 
  
+
+// let s = 'lokesh';
+// let arr = s.split('');
+// console.log(arr);
+
+
+// const button = document.querySelectorAll('button');
+// const body = document.querySelector('body');
+
+//  button.forEach(function (button) 
+//  {
+//     console.log(button);
+//     button.addEventListener('click' ,function (e) {
+//         console.log(e);
+//         console.log(e.target);
+//         if (e.target.id ==='grey') {
+//             body.style.backgroundColor = e.target.id 
+//         }
+        
+//         if (e.target.id ==='blue') {
+//             body.style.backgroundColor = e.target.id 
+//         }
+        
+//         if (e.target.id ==='yellow') {
+//             body.style.backgroundColor = e.target.id 
+//         }
+        
+//         if (e.target.id ==='white') {
+//             body.style.backgroundColor = e.target.id 
+//         }
+        
+//     })
+    
+//  });
+
+// console.log("hello");
+// let a1 = prompt("enter first number","");
+// let b1 = prompt("enter second number","");
+
+// let c1 = parseInt(a1) + parseInt(b1);
+// console.log(c1);
+// document.getElementById("add").innerHTML = c1
+
+
+// var a = prompt('enter first value' ,'');
+// a=parseInt(a)
+// var b = Math.sqrt(a);
+// document.getElementById("output").innerHTML = b
+// console.log(b);
+
+// let x =10;
+// let y = 5;
+// let z =10;
+// let s =(x+y+z)/2;
+// let temp = s*(s-x)*(s-y)*(s-z);
+// let area =Math.sqrt(temp);
+// console.log(area);
+// document.getElementById("data").innerHTML = area
+
+// let a = prompt('enter the a value');
+// let b = prompt('enter the b value');
+// console.log(`the value of a is:${a}, the value of b:${b}`);
+// let temp;
+// temp = a;
+// b= temp;
+// console.log(`after swapping\n the value of a:${a} the value of b:${b}`);
+// document.getElementById("val").innerHTML=`after swapping\n the value of a:${a} the value of b:${b}`;
+
+
+
+// var Bar = Function Foo()
+// {
+
+// return 11;
+
+// };
+
+// typeof Foo();
+
+
+
+// var a = 10;
+
+// if (function abc(){})
+
+// {
+
+// a += typeof abc;
+
+// }
+
+// console.log(a);
+
+
+
+
+// let number = prompt("enter a number");
+// number = parseInt(number)
+// let fact = 1;
+// if(number==0){
+//     console.log(`the factorial of ${number} is 1`);
+// }
+// else if (number<0){
+// console.log(`the factorial of -ive number is not possible`);
+// }
+// else{
+//         for(let i=1;i<=number;i++){
+//             fact = fact*i;
+      
+//         }
+//         console.log(`the factorial of ${number} is ${fact}`);
+//     }
+
+//     document.getElementById("val").innerHTML =` the factorial of ${number} is ${fact}` 
+
+
+
+
+
+// function getLetter(s) {
+//     let letter=prompt("Enter the value");
+//     // Write your code here
+//     switch(s.charAt(0)){
+//         case {a,e,i,o,u}:
+//         return "A";
+//         break;
+//         case 'b':
+//         case 'c':
+//         case 'd':
+//         case 'f':
+//         case 'g':
+//         return "B";
+//         break;
+//         case 'h':
+//         case 'j':
+//         case 'k':
+//         case 'l':
+//         case 'm':
+//         return "C";
+//         break;
+//         default:
+//         return "D";
+//     }
+// }
+
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+//   }
+  
+//   console.log(reverseString('Hello'));
+  // Output: 'olleH'
+
+// function reveString(value){
+//     return value.split('').reverse().join('');
+
+// }
+
+// console.log(reveString('lokesh'));
+
+// function removeDuplicates(arr) {
+//     return [...new Set(arr)];
+//   }
+  
+//   console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5,6,6,7,7,7,8,9,9,9]));
+
+
+//   function findMissingNumber(arr) {
+//     const n = arr.length + 1;
+//     const sum = (n * (n + 1)) / 2;
+//     const arrSum = arr.reduce((acc, curr) => acc + curr, 0);
+  
+//     return sum - arrSum;
+//   }
+  
+//   console.log(findMissingNumber([1, 2, 3, 5]));
+
+//   function findMaxNumber(arr) { 
+//     // arr=parseInt(arr)
+
+//     return Math.max.apply(null,arr); 
+  
+//   } 
+//   console.log(findMaxNumber([21,23,34,45,56,77]));
+
+//   function findMinMax(arr) { 
+
+//     let min = Math.min(...arr); 
+  
+//     let max = Math.max(...arr); 
+  
+//     return [min, max]; 
+  
+//   }
+//   console.log(findMinMax([1,3,5,6,8,9]));
+
+//   function factorial(number) { 
+
+//     if (number === 0 || number === 1) return 1; 
+//   (
+//     return number * factorial(number-1); 
+  
+//   } 
+// console.log(factorial(10));
+
+
+
+
+// function ascendingSort(numbers) { 
+
+//     return numbers.sort((a, b) => b-a); 
+
+  
+//   }
+
+// console.log(ascendingSort([1,2,3,4,5,6]));
+
+
+// function isPalindrome(str) { 
+
+//     return str === str.split('').reverse().join(''); 
+  
+//   }
+// console.log(isPalindrome('lokesh'));
+// console.log(isPalindrome('hello'));
+// console.log(isPalindrome('radar'));
+
+
+
+
+// function filterEvenNumbers(numbers){
+//     return numbers.filter(num=>num%2===0);
+// }
+// console.log(filterEvenNumbers([3,5,7,9,11,13,15,17,19,22,24,26,28]));
+
+
+// function isPrime(num) { 
+
+//     if (num <= 1) return false; 
+  
+//     for (let i = 2; i <= Math.sqrt(num); i++) { 
+  
+//       if (num % i === 0) return false; 
+  
+//     } 
+  
+//     return true; 
+  
+//   }
+//   console.log(isPrime(11));
+//   console.log(isPrime(31));
+//   console.log(isPrime(17));
+//   console.log(isPrime(21));
+
+
+
+//   function toTitleCase(str) { 
+
+//     return str.replace(/\b\w/g, l => l.toUpperCase()); 
+  
+//   } 
+  
+// console.log(toTitleCase('lokesh,singh'));
+
+// const inputString = "hello world";
+// const titleCaseString = toTitleCase(inputString);
+
+// console.log(titleCaseString);
+
+
+
+// function findLargestElement(nestedArray) {
+//     let largest = nestedArray[0][0];
+  
+//     for (let arr of nestedArray) {
+//       for (let num of arr) {
+//         if (num > largest) {
+//           largest = num;
+//         }
+//       }
+//     }
+  
+//     return largest;
+//   }
+
+//   const nestedArray = [
+//     [3, 8, 2],
+//     [12, 5, 7],
+//     [9, 4, 18]
+//   ];
+  
+//   const result = findLargestElement(nestedArray);
+//   console.log(result);
+  
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+
+
+// (function (){
+//   let a=b=9
+// })();
+// console.log(typeof a);
+// console.log(typeof b);
+
+// for(var i=0; i<5; i++){
+//   setTimeout(() => {console.log(i);
+    
+//   }, 2);
+ 
+// }
+
+// console.log(+true);
+// console.log(typeof +true);
+// console.log(!"lokesj");
+// console.log(typeof ("lokesj"));
+
+
+
+// console.log("start");
+
+// setTimeout(() => {
+//   console.log("2 second");
+// }, 2);
+
+// setTimeout(() => {
+//   console.log("0 second");
+// }, 0);
+
+// console.log("end");
+
+
+
+
+
+
+
+// let a= list.filter((x)=>x.age<18).map((x)=>x.name)
+// console.log(a[0]);
+
+// console.log("count of people with age<18" +" " +a.length);
+// let b= list.filter((x)=>x.age>18).map((x)=>x.name)
+// console.log(b);
+// console.log("count of people with age>18" +" "+ b.length);
+// let c= list.filter((x)=>x.age==18).map((x)=>x.name)
+// console.log(c);
+// console.log("count of people with age=18" +" "+ c.length);
+
+
+
+
+
+
+// let ages=[12,13,15,18,23,25,34];
+// ages.some(checkAdult);
+// function checkAdult(age) {
+//   return age > 18;
+// }
+
+// let a=ages.some((x)=>x.ages>=18)
+// console.log(a);
+
+// let numbers = [10, 12, 16, 14, 50];
+
+// let hasEvenNumber = numbers.every(function (num) {
+//   return num % 2 === 0;
+// });
+// let a=numbers.every((x)=>x%2==0)
+// console.log(a);
+// console.log(hasEvenNumber); // Output: true
+
+
+// let numbers = ['file1','file2','file3','file4','file5'];
+// numbers.copyWithin(0,4);
+// console.log(numbers);
+
+
+// let arr=[2,3,4,5,6,7]
+// arr.fill(9,2,5);
+// console.log(arr);
+
+
+
+// Custom function to find the last element that satisfies a condition
+
+
+// let numbers = [3,4,5,6,77,88,99,45,34,76,77];
+
+// let a=numbers.findLast((x)=>x>55)
+// console.log(a);
+
+// let arr=[3,4,5,6,7,8];
+// let j=arr.findLastIndex((r)=> r<6);
+// console.log(j);
+
+
+// let a=[3,5,6,7]
+// console.log(Array.isArray(a));
+
+// let ar=['apple','banana','orange','kiwi'];
+// let a=ar.join('+');
+// console.log(a);
+
+
+
+
+
+// let val = { name: 'John', age: 30, city: 'New York' };
+
+
+// let {name:n, age:a, city:l}=val;
+
+// console.log(n);  
+// console.log(a);   
+// console.log(l);  
+
+// let val=['jan','feb','mar','apri','jun'];
+// let result = val.with(4,'lokesj');
+// console.log(result);
+
+
+// let arr=[2,3,4,5,6];
+// let [a,b,...rest]=arr
+// console.log(a,b,rest);
+
+// var x;
+// var x=11;
+// console.log(x);
+
+
+
+// let mySet=new Set();
+// mySet.add(1);
+// mySet.add(2);
+// mySet.add(3);
+// mySet.add("lokesh");
+// mySet.add(4);
+// console.log (mySet.has(2));
+// console.log (mySet.delete("lokesh"));
+// console.log (mySet.has("lokesh"));
+
+//using for of loop
+
+// let array=[1,2,2,3,3,4,4,5,5,6,6,6,7,7];
+// let mySetArr= new Set(array);
+// for (let values of mySetArr) {
+//   console.log(values);
+  
+// }
+
+// console.log(mySetArr.size);
+//using set function 
+
+// let arr1=[1,1,2,2,3,3,4,4,5,5,5,6,6,7,7,8,8,8,9,9];
+// let myUniqueNum=[...new Set(arr1)];
+// console.log(myUniqueNum);
+
+// let myNane = 'lokesh kuntal';
+// console.log(new Set(myNane));
+
+//remove null and undefined
+
+// let obj={
+//   a:1,
+//   b:null,
+//   c:undefined,
+//   d:'hello'
+// }
+
+// let newObj= Object.fromEntries(Object.entries(obj).filter(([__,val])=>val !=null))
+// console.log(newObj);
+
+// let ArrData = (arr) => {
+//   return Object.fromEntries(arr)
+// }
+// console.log(ArrData([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+//   ['d', 4],
+//   ['e', 5]
+// ]))
+ 
+
+// let arr=[22,12,13,14,15,56,67,78];
+// let smallNumber=(arr)=>
+// arr.reduce((smallnum,num)=>Math.min(smallnum,num));
+// console.log(smallNumber(arr));
+
+// let arr=[22,12,13,14,15,56,67,78];
+// let smallNumber=(arr)=>
+// arr.reduce((smallnum,num)=>Math.max(smallnum,num));
+// console.log(smallNumber(arr));
+
+
+// let arr1=[1,1,2,2,3,3,4,4,5,5,5,6,6,7,7,8,8,8,9,9];
+// let myUniqueNum=[...new Set(arr1)];
+// console.log(myUniqueNum);
+
+
+// let arr1=[1,3,4,5,5],
+//     arr2=[2,4,4,6,8]
+
+// let arr3=[...arr1,...arr2];
+// // console.log(arr3); 
+
+// arr3.sort((a,b) => a-b);
+// // console.log(arr3); 
+
+// arr3=[...new Set(arr3)];
+// console.log(arr3); 
+
+//swap two number using by destructureing
+// let num1=50;
+// let num2=60;
+// [num1,num2]=[num2,num1]
+// console.log(num1,num2);
+
+
+
+// let arr1=[1,2,3,4,5]
+// let arr2=[3,4,5,6,7]
+// let inArr = arr1.filter((currebtElem)=>{
+//   return arr2.includes(currebtElem);
+// });
+// console.log(inArr);
+
+// let arr1=[1,2,3,4,5]
+// let arr2=[3,4,5,6,7]
+// let unionArr=[...new Set([...arr1,...arr2])];
+// console.log(unionArr);
+
+
+// console.log(400.4=== 200.2*2);
+// console.log(900.9=== 300.3*3);
+
+////// 07/02/24 date Question answer
+
+// let list =[
+//   {name:'lokesh',age:32},
+//   {name:'ram',age:21},
+//   {name:'seeta',age:15},
+//   {name:'khushi',age:18},
+//   {name:'deepak',age:17},
+//   {name:'vishal',age:23},
+//   {name:'priya',age:22},
+//   {name:'isha',age:12},
+// ]
+// isMinor used in array of Object
+
+// list.map((x)=>x.isMinor=x.age>18?true:false);
+// console.log(list);
+// list.map((x)=>x.isMinor=x.age<18?true:false);
+// console.log(list);
+
+// Removing the age column
+// let nameOnly = list.map((item)=>item.name)
+// console.log(nameOnly);
+
+
+// let value=[
+// {name:'ram',age:24},
+// {name:'shyam',age:23},
+// {name:'geeta'},
+// {name:'seeta',age:18},
+// {name:'lokesh'},
+// ]
+/// which name have no age 
+// let Name = value.filter((item)=> {
+//   return item.age === undefined || item.age === null;
+// }).map((item)=> {
+//   return item.name;
+// });
+// console.log(Name);
+
+// let value = (num)=>{
+//   let myRev = num.toString().split("").reverse().join("");
+//   console.log(myRev);
+// };
+
+// value(12334567);
+
+
+// let obj ={a:1,b:2,c:3};
+// let {a,b,c} = obj;
+// let updatedObj={b:8};
+// console.log(updatedObj);
+
+// let val={x:1,y:2};
+// let{x,y}=val;
+// let newVal={c:6};
+// console.log(newVal);
+
+// const obje = { a: 1, b: 2, c: 3,d:9,e:8,f:7 };
+
+// delete obje.b;
+// delete obje.e;
+// delete obje.f;
+// console.log(obje);
+
+// let findVowel=()=>{
+//   let char="O";
+//   char=char.toLocaleLowerCase();
+//   if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
+//     console.log("chr is vowel");
+//   } else{
+//     console.log("char is consonent");
+//   }
+// };
+// findVowel();
+
+
+// let num = 100;
+// let x=0;
+// let y=1;
+// let fn=x+y;
+// console.log(x);
+// while(fn<num){
+//   console.log(fn);
+//   fn=x+y;
+//   x=y;
+//   y=fn;
+// }
+
+// let arr=[22,3,4,5,67];
+// let sum = arr.reduce((accum,current)=>{
+//   return accum*current;
+// })
+// console.log(sum);
+
+
+// let arr=[5,8,2,5,10];
+// let avg = arr.reduce((accum,current,index,array)=>{
+//   let total=accum+current;
+//   if(index==array.length-1){
+//     return total/array.length;
+//   }
+//     return total
+
+// })
+// console.log(avg);
+
+// let add=(a,b)=>{
+//   return a+b;
+// }
+
+// let subs=(a,b)=>{
+//   return a-b;
+// }
+
+// let mult=(a,b)=>{
+//   return a*b;
+// }
+
+// let cal=(num1,num2,operator)=>{
+//   return operator(num1,num2);
+// }
+// console.log(cal(2,3,add));
+
+
+// let val=[
+//   ['zone_1','zone_2'],
+//   ['zone_3','zone_4',['zone_5',['zone_6',['zone_7']]]]
+// ];
+// console.log(val.flat(Infinity));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
